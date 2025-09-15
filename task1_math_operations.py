e87f9b06d6225f39d61b6cf9d9cd67b3cf860f78
Rishiraj singh chouhan <chouhanrishirajsingh321@gmail.com>
2025-09-15 22:49:49 +0530

    Create task1_math_operations.py

---
+++ task1_math_operations.py
@@ -0,0 +1,13 @@
+# Task 1: Perform Basic Mathematical Operations
+
+# Taking two numbers as input
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+
+# Displaying results
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
+
